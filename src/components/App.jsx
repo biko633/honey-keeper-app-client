4f3060dfc2ff465773fb454138d49d210e0a4d22
biko <ahmadyaslam925@gmail.com>
2024-03-16 14:47:43 +0300

    change file name try fit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "../pages/home.jsx";
-import { UserForm } from "../pages/userform.jsx";
+import { UserForm } from "../pages/UserForm.jsx";
 import { ForgotPassword } from "../pages/forgotpassword.jsx";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
